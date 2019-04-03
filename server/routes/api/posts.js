9baccb7ca7d59f5c4fbeb3539628c0b71fe417f8
Valentin Lazurca <lazurcavalentin@gmail.com>
2019-04-03 17:40:58 +0300

    initial

---
+++ server/routes/api/posts.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+// GET posts
+router.get('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    res.send(await posts.find({}).toArray());
+});
+
+// ADD posts
+router.post('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.insertOne({
+        text: req.body.text,
+        isChecked: req.body.isChecked
+    });
+    res.status(200).send();
+});
+
+// UPDATE posts
+// router.put('/:id', async (req, res) => {
+//     const posts = await loadPostsCollection();
+//     await posts.updateOne({
+//         isChecked: req.body.isChecked
+//     });
+//     res.status(200).send(res);
+// });
+
+// DELETE posts
+router.delete('/:id', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id)});
+    res.status(200).send();
+});
+
+
+async function loadPostsCollection() {
+    const client = await mongodb.MongoClient.connect('mongodb+srv://vali123:vali123@vueproject-sfur0.mongodb.net/test?retryWrites=true', {
+        useNewUrlParser : true
+    });
+
+    return client.db('vueproject').collection('posts');
+}
+
+module.exports = router;
